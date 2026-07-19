b53af33277adb7f61e29f28ab031a4faf71384c0
xaruki <112648665+Hantkyaw@users.noreply.github.com>
2026-07-19 19:53:20 +0630

    Update app.py

--- app.py
+++ app.py
@@ -140,5 +140,5 @@ with gr.Blocks(title="Structure Generator Tool") as demo:
     )
 
 if __name__ == "__main__":
-    demo.launch(theme=gr.themes.Soft())
-  
+    demo.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 7860)))
+
